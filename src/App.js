0e972573bb96d1d145c2a12e3d9c1bbfe23c54d8
Inwoo Jeong <sinth92@hotmail.com>
2021-09-28 10:20:05 +0900

    props 학습

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-function Food(){
-  return <h1>I like potato</h1>;
+function Food({fav}){
+  return <h1>I like {fav}</h1>;
 }
 
 function App() {
   return (
   <div>
     <h1>Hello</h1>
-    <Food/>
+    <Food fav="Kimchi"/>
+    <Food fav="samgiopsal"/>
+    <Food fav="chukumi"/>
   </div>
   );
 }
